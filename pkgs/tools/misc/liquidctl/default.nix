9f8a37db92654e9cba5274bc2a73d0d79b8479c5
anirrudh <anik597@gmail.com>
2020-04-18 23:17:35 -0500

    cleaned up default.nix

--- pkgs/tools/misc/liquidctl/default.nix
+++ pkgs/tools/misc/liquidctl/default.nix
@@ -1,15 +1,17 @@
 { stdenv, python3Packages }:
 
-python3Packages.buildPythonApplication rec {
+with python3Packages;
+
+buildPythonApplication rec {
   pname = "liquidctl";
   version = "1.3.3";
 
-  src = python3Packages.fetchPypi {
+  src = fetchPypi {
     inherit pname version;
     sha256 = "d13180867e07420c5890fe1110e8f45fe343794549a9ed7d5e8e76663bc10c24";
   };
 
-  buildInputs = with python3Packages; [
+  buildInputs = [
     pyusb
     hidapi
     docopt
