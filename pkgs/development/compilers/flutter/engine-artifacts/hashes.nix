2cc06b65229c3b63bb2006548f500d5ec19c8407
anirrudh <anirrudh@protonmail.com>
2023-12-20 00:25:49 -0500

    asd

--- pkgs/development/compilers/flutter/engine-artifacts/hashes.nix
+++ pkgs/development/compilers/flutter/engine-artifacts/hashes.nix
@@ -50,7 +50,7 @@
     darwin-arm64 = {
       "artifacts.zip" = "sha256-kdgsj89zA17OernmXyomI2wfTSUJxb+lubr4AHXrYik=";
       "font-subset.zip" = "sha256-NEN4x96RyRvJnQMuQjmQddIm+3aWzuIwmmg1jRmwmOA=";
-    }; # fixed till here
+    }; 
     darwin-arm64-profile = {
       "artifacts.zip" = "sha256-Rqp5b3StavBF+pwn8MPgZgUNOYtizxp0y+jP2hlPETw=";
     };
@@ -63,7 +63,7 @@
       "artifacts.zip" = "sha256-1TjPYPGPw9AJ/mvB7dO7kfvfNgGyOe5DX8WDPC9JWS0=";
       "font-subset.zip" = "sha256-BL7xU+pyQMW/EB05X5A3Dkrz+46v8PPUauGoO+KeyBc=";
       "gen_snapshot.zip" = "sha256-HSRasNmoQLnhNdvtKKNeJn4x3EAvltLb0M+IzoDsWnw=";
-    }; # mark 2
+    }; 
     darwin-x64-profile = {
       "FlutterMacOS.framework.zip" = "sha256-kw4esoj9mubPEGE4OokP7a7QEMFzUPAmutxORJWZndM=";
       "artifacts.zip" = "sha256-iUX2Psy8n199O/jVHEhhzuZ7RcQBTbdMKMGn5Dtc3SI=";
@@ -76,7 +76,7 @@
       "gen_snapshot.zip" = "sha256-y2zl/Z6hWgqQ3D4uk2CHqtrw/wjix1un+fAf53gzzA8=";
     };
     "flutter_patched_sdk.zip" = "sha256-SjGccH51cUF8EEmzSQRxHrgHhB27QniptNOoSB4kaLQ=";
-    "flutter_patched_sdk_product.zip" = "sha256-BBqeN34ZATXH9A56eg4xN38TfpXdUeTfh0bYkY/Pyp0="; # mark 3
+    "flutter_patched_sdk_product.zip" = "sha256-BBqeN34ZATXH9A56eg4xN38TfpXdUeTfh0bYkY/Pyp0="; 
     ios = {
       "artifacts.zip" = "sha256-jLc6AlV8ZGWNgZ8Ss4NS2jp5HUH2RJLS1qpJNyqWWXM=";
     };
@@ -84,9 +84,9 @@
       "artifacts.zip" = "sha256-ENeZF3H0X+3YZMvPRFgl+CiiSiFyvgaQvBSCZphMUVY=";
     };
     ios-release = {
-      "Flutter.dSYM.zip" = "sha256-0YqlX4QhxnDb9LasMcBcPO/+30LCfVbwC+z+wZiiEqk=";
+      "Flutter.dSYM.zip" = "sha256-2Jkuo+sdTCVSspi2HVHoRG0qZ5KWumKD8WFWDiDGMNA=";
       "artifacts.zip" = "sha256-8z+TrpqWeyzZqV3fHGmzA1dVJHq8D/ORcWZ2v+C6g8g=";
-    }; # mark 4
+    }; # fixed till here
     linux-arm64 = {
       "artifacts.zip" = "sha256-djesma+IqQZgGlxQj4Gv6hAkQhQKQp7Gsa1I4hksqNc=";
       "font-subset.zip" = "sha256-Wo11dks0uhLI2nu+9QJ7aLmvfsPcuqvcmquak4qv5XM=";
